0d12ca809bf53e16fe5ca1dfaaa0afa6cd51d8ab
amagalla <anthonymagallanes24@gmail.com>
2021-03-11 20:37:32 -0800

    Implemented clustering for zero-downtime and auto load-balancing

--- server/server.js
+++ server/server.js
@@ -2,16 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const passport = require("passport");
-const cookieParser = require('cookie-parser');
-const cors = require('cors')
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
 
 app.use(cookieParser());
-app.use(cors())
+app.use(cors());
 
 //import routers
 const passportRouter = require("./routes/passportRouter");
 const secretRouter = require("./routes/secretRouter");
-const userRouter = require('./routes/userRouter')
+const userRouter = require("./routes/userRouter");
 
 require("dotenv").config();
 const { PORT } = process.env;
@@ -44,4 +46,21 @@ app.use((err, req, res, next) => {
   return res.status(errorObj.status).json(errorObj.message);
 });
 
-app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
+// checks if cluster is the master cpu core
+if (cluster.isMaster) {
+  // spawn workers up to amount of cpu cores.
+  for (let i = 0; i < numCPUs; i++) {
+    // spawns a new worker
+    cluster.fork();
+  }
+  // catch if worker had died
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    // create a new worker
+    cluster.fork();
+  });
+} else {
+  app.listen(3000, () =>
+    console.log(`Server ${process.pid} running at port 3000`)
+  );
+}
