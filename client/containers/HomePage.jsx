2e6564f6ef2e406498f9c131b3859bc83d9912d1
amagalla <anthonymagallanes24@gmail.com>
2021-04-09 22:54:40 -0700

    Commented out Redis. Included Readme

--- client/containers/HomePage.jsx
+++ client/containers/HomePage.jsx
@@ -1,38 +1,41 @@
-import React, { useState, useEffect, Suspense } from 'react'
-// import Map from '../components/Map'
-import {Redirect, useLocation} from 'react-router-dom'
-import Header from '../components/Header'
+import React, { useState, useEffect, Suspense } from "react";
+import Map from "../components/Map";
+import { Redirect, useLocation } from "react-router-dom";
+import Header from "../components/Header";
 
-const Map = React.lazy(() => import('../components/Map'))
+// const Map = React.lazy(() => import('../components/Map'))
 const HomePage = () => {
-    const location = useLocation()
-    // console.log('LOCATION', location.state.userID)
-    const [coord, setCoord] =  useState({})
+  const location = useLocation();
+  // console.log('LOCATION', location.state.userID)
+  const [coord, setCoord] = useState({});
 
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(function(position) {
-            console.log("Latitude is :", position.coords.latitude);
-            console.log("Longitude is :", position.coords.longitude);
-            setCoord({latitude: position.coords.latitude, longitude: position.coords.longitude})
-          });
-    },[])
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+      setCoord({
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      });
+    });
+  }, []);
 
-
-    console.log('position.coords.lat ', coord)
-    console.log(location)
-    return (
-        <div className="home">
-        { 
-        location.state !== undefined ?
-        <Suspense fallback={<div className="loading"></div>}>
-            <Header userID={location.state.userID} />
-            <Map coord={coord} userID={location.state.userID}/>
-        </Suspense>
-        :
-        <Redirect to='/'/>
-        }
-        </div>
-    )
-}
+  console.log("position.coords.lat ", coord);
+  console.log(location);
+  return (
+    <div className='home'>
+      {location.state !== undefined ? (
+        // <Suspense fallback={<div className="loading"></div>}>
+        <>
+          <Header userID={location.state.userID} />
+          <Map coord={coord} userID={location.state.userID} />
+          {/* </Suspense> */}
+        </>
+      ) : (
+        <Redirect to='/' />
+      )}
+    </div>
+  );
+};
 
 export default HomePage;
